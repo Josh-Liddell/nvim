2c97a1aa184393170f145ce5a68f1b734819a3d9
Josh-Liddell <joshua.liddell21@gmail.com>
2025-02-16 12:04:13 -0700

    Added nvim-cmp

--- init.lua
+++ init.lua
@@ -9,7 +9,7 @@ vim.opt.ruler = false -- Removes the bottom right hand info
 vim.opt.termguicolors = true
 
 -- For python development
-vim.g.python3_host_prog = '~/.venv/bin/python'
+vim.g.python3_host_prog = '~/.venv/bin/python3'
 
 require("config.lazy")
 require("config.keymaps")
@@ -22,6 +22,12 @@ vim.opt.laststatus = 3 --This sets the status bar to global so that it doesn't s
 -- This is how I could change the colors of certain things
 -- vim.cmd [[hi @function.builtin guifg=blue]]
 
+-- vim.cmd([[
+--     :hi      NvimTreeExecFile    guifg=#ffa0a0
+--     :hi      NvimTreeSpecialFile guifg=#ff80ff gui=underline
+--     :hi      NvimTreeSymlink     guifg=Yellow  gui=italic
+--     :hi link NvimTreeImageFile   Title
+-- ]])
 
 -- Highlight when yanking (copying) text
 --  Try it with `yap` in normal mode
@@ -31,6 +37,13 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
+  end,
+})
+
+-- This restores a blinking cursor when I exit
+vim.api.nvim_create_autocmd("VimLeave", {
+  callback = function()
+    vim.cmd("set guicursor=a:ver25-blinkwait700-blinkon400-blinkoff250")
   end,
 })
 
